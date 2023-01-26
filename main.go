8c94f7687e72793ddd0622ff0b3232fa6745cd92
NathanMayPro <nathan.may.pro@gmail.com>
2023-01-26 19:40:14 +0100

    fixed middleware handler communication

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ type Document struct {
 	Uid       string `json:"uid"`
 }
 
+type Response struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	//Data    interface{} `json:"data"`
+}
+
 // decoding b64 encoded document
 func decodeb64(Content string) []byte {
 	// decode base64 string
@@ -57,7 +63,8 @@ func save(w http.ResponseWriter, r *http.Request) {
 
 	// if path is not empty
 	if path == "" {
-		log.Fatal("path is empty.")
+		log.Printf("endpoint:homepage hit %v", r.RemoteAddr)
+		return
 	}
 
 	// Declare a new Document struct.
@@ -85,17 +92,29 @@ func save(w http.ResponseWriter, r *http.Request) {
 	log.Printf("save endpoint done %v", r.RemoteAddr)
 }
 
-func retrieve(w http.ResponseWriter, r *http.Request) {
+func retrieve(w http.ResponseWriter, r *http.Request) (Response, error) {
 	// log activity
 	log.Printf("retrieve endpoint hit %v", r.RemoteAddr)
 
+	//init response
+	//var response Response
+
 	// check if there is a path as query parameter
 	path := r.URL.Query().Get("path") // http://localhost:8080/retrieve?path=/home/username
-
-	// if path is not empty
-	if path == "" {
-		logAndKill(nil, "path is empty.")
-	}
+	// w = context.WithValue(w, "result", v)
+	// if path is empty
+	//if path == "" {
+	// ctx := r.Context()
+	// req := r.WithContext(context.WithValue(ctx, "error", "Something went wrong"))
+	// //req =
+	// *r = *req
+	// to pass the context to the middleware
+	//responseMiddleware(http.HandlerFunc(homepage)).ServeHTTP(w, r)
+	return Response{
+		Status:  400,
+		Message: "path is empty",
+	}, nil // to stop the execution of the function
+	//}
 
 	// check if file exists
 	fileInfo, error := os.Stat(path)
@@ -110,18 +129,61 @@ func retrieve(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, path)
 
 	log.Printf("retrieve endpoint done %v", r.RemoteAddr)
+	return Response{
+		Status:  200,
+		Message: "success",
+	}, nil
+
 }
 
 func logAndKill(err error, msg string) {
 	log.Printf("failed to %s: %v", msg, err)
-	panic(err)
-
 }
 
 func must(err error, msg string) {
 	if err != nil {
 		logAndKill(err, msg)
 	}
+}
+
+type customHandler func(w http.ResponseWriter, r *http.Request) (Response, error)
+
+func responseMiddleware(next customHandler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Define a struct to hold the response
+		//var response Response
+		// Call the next handler and capture the response
+		response, err := next(w, r)
+		if err != nil {
+			json.NewEncoder(w).Encode(Response{
+				Status:  500,
+				Message: "internal server error",
+			})
+		} else {
+			json.NewEncoder(w).Encode(Response{
+				Status:  response.Status,
+				Message: response.Message,
+			})
+		}
+		// // Set the response struct with the desired values
+		// // check inside the context if there is an error
+		// if err := r.Context().Value("error"); err != nil {
+		// 	fmt.Print("I'm here")
+		// 	response.Status = r.Context().Value("status").(int)
+		// 	response.Message = err.(string)
+		// 	// Write the response as JSON
+		// 	w.Header().Set("Content-Type", "application/json")
+		// 	json.NewEncoder(w).Encode(response)
+		// 	return
+		// } else {
+		// 	fmt.Print("I'm also here")
+		// 	response.Status = 200
+		// 	response.Message = "success"
+		// 	// Write the response as JSON
+		// 	w.Header().Set("Content-Type", "application/json")
+		// 	json.NewEncoder(w).Encode(response)
+
+	})
 }
 
 func main() {
@@ -136,9 +198,9 @@ func main() {
 	defer file.Close()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", homepage)
-	mux.HandleFunc("/retrieve", retrieve)
-	mux.HandleFunc("/save", save)
+	// mux.Handle("/", responseMiddleware(homepage))
+	// mux.Handle("/save", responseMiddleware(http.HandlerFunc(save)))
+	mux.Handle("/retrieve", responseMiddleware(retrieve))
 
 	// log activity
 	log.Printf("Server started on port 8080\n")
