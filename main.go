db5754d507dc2396cf07b5af47e115ed42303195
NathanMayPro <nathan.may.pro@gmail.com>
2023-01-26 16:08:39 +0100

    add logging

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -20,42 +19,38 @@ type Document struct {
 func decodeb64(Content string) []byte {
 	// decode base64 string
 	decoded, err := base64.StdEncoding.DecodeString(Content)
-	if err != nil {
-		log.Fatal(err)
-	}
+	must(err, "decode base64 string")
+
+	// check if decoded is empty
 	if decoded == nil || len(decoded) == 0 {
-		log.Fatal("Something went wrong while decoding base64 string.")
+		logAndKill(err, "decode base64 string")
 	}
 	return decoded
 }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+	log.Printf("endpoint:homepage hit %v", r.RemoteAddr)
 }
 
 func save_file(decoded []byte, filename string, extension string, path string) error {
 	// create file
 	file, err := os.Create(path + filename + "." + extension)
-	if err != nil {
-		log.Fatal(err)
-	}
+	must(err, "create file")
 
 	// write to file
 	_, err = file.Write(decoded)
-	if err != nil {
-		log.Fatal(err)
-	}
+	must(err, "write to file")
 
 	// close file
 	err = file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	must(err, "close file")
 
 	return err
 }
 
 func save(w http.ResponseWriter, r *http.Request) {
+	// log activity
+	log.Printf("save endpoint hit %v", r.RemoteAddr)
 
 	// check if there is a path as query parameter
 	path := r.URL.Query().Get("path") // http://localhost:8080/save?path=/home/username
@@ -84,40 +79,70 @@ func save(w http.ResponseWriter, r *http.Request) {
 
 	// save file
 	err = save_file(decoded, d.Filename, d.Extension, path)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fmt.Printf("Log: save endpoint hit %v", path)
+	must(err, "save file")
 
+	log.Printf("save endpoint done %v", r.RemoteAddr)
 }
 
 func retrieve(w http.ResponseWriter, r *http.Request) {
+	// log activity
+	log.Printf("retrieve endpoint hit %v", r.RemoteAddr)
 
-	fmt.Println("retrieve endpoint hit")
 	// check if there is a path as query parameter
 	path := r.URL.Query().Get("path") // http://localhost:8080/retrieve?path=/home/username
 
 	// if path is not empty
 	if path == "" {
-		log.Fatal("path is empty.")
+		logAndKill(nil, "path is empty.")
+	}
+
+	// check if file exists
+	fileInfo, error := os.Stat(path)
+	must(error, "check if file exists")
+
+	// check if file is a directory
+	if fileInfo.IsDir() {
+		logAndKill(nil, "file is a directory")
 	}
 
 	// open file using READ & WRITE permission
 	http.ServeFile(w, r, path)
+
+	log.Printf("retrieve endpoint done %v", r.RemoteAddr)
+}
+
+func logAndKill(err error, msg string) {
+	log.Printf("failed to %s: %v", msg, err)
+	panic(err)
+
+}
+
+func must(err error, msg string) {
+	if err != nil {
+		logAndKill(err, msg)
+	}
 }
 
 func main() {
+	// set up the log file
+	file, err := os.OpenFile("./logging/api.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+
+	must(err, "open log file")
+
+	// set the output of the logs to be the file
+	log.SetOutput(file)
+
+	defer file.Close()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homepage)
 	mux.HandleFunc("/retrieve", retrieve)
 	mux.HandleFunc("/save", save)
 
 	// log activity
-	fmt.Printf("Server started on port 8080\n")
+	log.Printf("Server started on port 8080\n")
 
-	//mux.HandleFunc("/person/list", personList)
-
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	errServer := http.ListenAndServe(":8080", mux)
+	must(errServer, "start server")
 }
